d248b25f8b94c445ef97a13bb8cd529c1e3c8110
Anton Martinsson <aejmartinsson@gmail.com>
2021-05-30 14:32:18 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -11,13 +11,12 @@ let package = Package(
         )
     ],
     dependencies: [
-		.package(name: "Plot", url: "https://github.com/johnsundell/plot.git", from: "0.9.1"),
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
     ],
     targets: [
         .target(
             name: "Portfolio",
-            dependencies: ["Plot", "Publish"]
+            dependencies: ["Publish"]
         )
     ]
 )
